9d1e09cca1d410bc76c86fb7bc85c6b5557833b0
Urieldev <urieel.mm@gmail.com>
2022-08-24 12:35:32 -0500

    Feat: se agrego useFetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
 import Layout from "./components/Layout/Layout";
+import useFetch from "./hooks/useFetch";
 
 function App() {
-  return <Layout></Layout>;
+  const { result, loading, error } = useFetch(
+    "http://hp-api.herokuapp.com/api/characters"
+  );
+  console.log(result[0].name);
+  return (
+    <Layout>
+      <div>
+        {result.map((character) => {
+          return <p style={{ color: "#FFFFFF" }}>{character.name}</p>;
+        })}
+      </div>
+    </Layout>
+  );
 }
 
 export default App;
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,1 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const useFetch = (URL) => {
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(URL);
+        const data = await res.json();
+        setResult(data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        setError(error.message);
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
+  return {
+    result,
+    loading,
+    error,
+  };
+};
+
+export default useFetch;
