5ed96e7f8d05657321a3586a465c35ede3e6d0cb
Urieldev <urieel.mm@gmail.com>
2022-08-23 23:30:06 -0500

    Fixed: correcciones en el header mobile

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import save_fill from "../../assets/save_fill.svg";
+import user_fill from "../../assets/user_fill.svg";
 
 const Menu = () => {
   return (
     <div className="menu-container">
-      <div className="menu-option menu-option--left">
+      <button className="menu-option menu-option--left">
         <p style={{ color: "#FFFFFF" }}>Favoritos</p>
-        <span>i</span>
-      </div>
-      <div className="menu-option menu-option--right">
+        <img
+          src={save_fill}
+          alt="Icono favoritos"
+          className="menu-option--icon"
+        />
+      </button>
+      <button className="menu-option menu-option--right">
         <p>Agregar</p>
-        <span>i</span>
-      </div>
+        <img
+          src={user_fill}
+          alt="Icono agregar"
+          className="menu-option--icon"
+        />
+      </button>
     </div>
   );
 };
